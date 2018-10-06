db5bbe0e741df7c6aad7bdbbded59fdefb31fc49
Sung Bae <singsang2@gmail.com>
2018-10-06 21:09:32 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    string = array.first.to_s
+  elsif array.length == 2
+    string= (array.first.to_s + " and " +array.last.to_s)
+  else
+    last = array.pop()
+    string = array.join(", ")
+    string += ", and #{last}"
+  end
+  string
 end
